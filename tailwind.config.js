c0968efdb92f94bb927cd3460403c4a552abae12
ryota-murakami <dojce1048@gmail.com>
2021-12-17 00:07:44 +0900

    migrate tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
