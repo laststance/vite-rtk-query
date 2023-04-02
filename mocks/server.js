0a8622a650c7c631bf8a909c38c511e04053421d
ryota-murakami <dojce1048@gmail.com>
2023-04-02 19:26:33 +0900

    move jsdom polyfill to setupTest

--- mocks/server.js
+++ mocks/server.js
@@ -1,10 +1,5 @@
 // from https://github.com/mswjs/examples/blob/master/examples/with-jest/src/mocks/server.js
 import { setupServer } from 'msw/node'
-import { Request, Headers } from 'node-fetch'
 
 import { handlers } from './handlers'
-
-globalThis.Request = Request
-globalThis.Headers = Headers
-
 export const server = setupServer(...handlers)
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,13 @@
 import matchers from '@testing-library/jest-dom/matchers'
+import { Headers, Request } from 'node-fetch'
 
+import { server } from '../mocks/server'
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
 
-import { server } from '../mocks/server'
+// @ts-ignore
+globalThis.Request = Request
+globalThis.Headers = Headers
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 
