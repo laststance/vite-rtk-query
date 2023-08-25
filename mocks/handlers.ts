356a5fa9f76dc51ab33de55927e585932474c9cb
ryota-murakami <dojce1048@gmail.com>
2023-08-25 16:38:48 +0900

    change docs

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -6,15 +6,19 @@ export const handlers = [
   rest.get('http://localhost:4000/api/docs_list', async (_req, res, ctx) => {
     const data = [
       { name: 'Vite', url: 'https://vitejs.dev/' },
+      { name: 'Redux Style Guide', url: 'https://redux.js.org/style-guide/' },
       { name: 'Redux Toolkit', url: 'https://redux-toolkit.js.org/' },
-      { name: 'React Router', url: 'https://reactrouter.com/en/main' },
       {
-        name: 'Redux First History',
-        url: 'https://github.com/salvoravida/redux-first-history',
+        name: 'RTK Query API Reference',
+        url: 'https://redux-toolkit.js.org/rtk-query/api/createApi',
       },
       { name: 'Vitest', url: 'https://vitest.dev/' },
       { name: 'MSW', url: 'https://mswjs.io/' },
       { name: 'Tailwind CSS', url: 'https://tailwindcss.com/' },
+      {
+        name: 'CSS Nesting',
+        url: 'https://developer.chrome.com/articles/css-nesting/',
+      },
       {
         name: 'CSS Modules',
         url: 'https://github.com/css-modules/css-modules',
