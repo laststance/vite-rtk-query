bbfe52456ef0f0c9072061b70d1912c121adb9b3
Ryota Murakami <dojce1048@gmail.com>
2025-05-29 04:38:45 +0900

    chore: update msw package and adjust configuration
    - Updated msw package version from 2.7.6 to 2.8.5 in package.json.
    - Modified msw.workerDirectory

--- mocks/browser.ts
+++ mocks/browser.ts
@@ -3,4 +3,4 @@ import { setupWorker } from 'msw/browser'
 import { handlers } from './handlers'
 
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers)
+export const worker = setupWorker(...(handlers as any[]))
